212aa20c9c608942ed8d50d53de720e916999f34
Jim Hough <teachlife@outlook.com>
2019-06-24 18:10:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,9 @@ class App < Sinatra::Base
   get '/new' do
     erb :create_puppy
   end
+
+  post '/puppy' do
+    puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    erb :display_puppy
+  end
 end
